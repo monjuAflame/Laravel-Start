799df29ec63542fc5a85af6a99f624b8cae5a623
monjuAflame <monjuaflame@gmail.com>
2021-09-01 14:56:38 +0600

    version laravel 8

--- public/js/app.js
+++ public/js/app.js
@@ -79783,8 +79783,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! G:\xampp\htdocs\laravue\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! G:\xampp\htdocs\laravue\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\xampp\htdocs\e-vape\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! E:\xampp\htdocs\e-vape\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
